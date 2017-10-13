95e2943a23a7155d3b4f8a56bc7056f627826250
bvi1994 <bvi1@ucsc.edu>
2017-10-13 10:51:06 -0700

    Completed Excerise A and B

--- maps/App.js
+++ maps/App.js
@@ -11,35 +11,105 @@ import {
 } from 'expo';
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.3382,
+        longitude: -121.8863,
+        latitudeDelta: .25,
+        longitudeDelta: .1
+      }
+    }
+  }
+
+  goistuanbul(){
+    let istuanbul = {
+      latitude: 41.067841,
+      longitude: 29.045258,
+      latitudeDelta: .25,
+      longitudeDelta: .1
+    }
+    this.setState({region: istuanbul})
+  }
+
+  gosyndey(){
+    let syndey = {
+      latitude: -33.866174,
+      longitude: 151.220345,
+      latitudeDelta: .25,
+      longitudeDelta: .1
+    }
+    this.setState({region: syndey})
+  }
+
+  gohongkong(){
+    let hongkong = {
+      latitude: 22.294074,
+      longitude: 114.171995,
+      latitudeDelta: .25,
+      longitudeDelta: .1
+    }
+    this.setState({region: hongkong})
+  }
+
+  getCurrent(){
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        let currentLocation = {
+          latitude: success.coords.latitude,
+          longitude: success.coords.longitude,
+          latitudeDelta: .25,
+          longitudeDelta: .1
+        }
+        this.setState({region: currentLocation})
+      },
+      (error) => {
+        alert("Unable to get current location! ")
+      },
+      {}
+    )
+  }
+
   render() {
     return (
       <View style={{
           flex: 1
         }}>
         <View style={{flex: 1, flexDirection: 'row'}}>
-          <TouchableOpacity
+          <TouchableOpacity onPress={() =>
+            this.goistuanbul()
+          }
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
               justifyContent: 'center'}}>
             <Text>Istanbul</Text>
           </TouchableOpacity>
-          <TouchableOpacity
+          <TouchableOpacity onPress={() => this.gosyndey()}
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
               justifyContent: 'center'}}>
             <Text>Sydney</Text>
           </TouchableOpacity>
-          <TouchableOpacity
+          <TouchableOpacity onPress={() => this.gohongkong()}
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
               justifyContent: 'center'}}>
             <Text>Hong Kong</Text>
           </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.getCurrent()}
+            style={{flex: 1,
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center'}}>
+            <Text>Current Location</Text>
+          </TouchableOpacity>
         </View>
-        <MapView style={{flex: 7}} />
+        <MapView style={{flex: 7}} region={this.state.region} />
       </View>
     );
   }
